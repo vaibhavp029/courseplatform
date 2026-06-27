eee0eea1612cabc0c84a65d093ea79e0e61c17ce
vaibhavp029 <vaibhavpanchal029@gmail.com>
2026-06-28 01:20:17 +0530

    Beginning this project and commit spring initilizer files

---
+++ src/main/java/com/agency/courseplatform/CourseplatformApplication.java
@@ -0,0 +1,13 @@
+package com.agency.courseplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseplatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agency/courseplatform/CourseplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agency.courseplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
