4972b2968910c0e99bf6cd8a2f31b2f7cdae9035
vaibhavp029 <vaibhavpanchal029@gmail.com>
2026-07-02 09:28:33 +0530

    Add UserRepository using Spring Data JPA

---
+++ src/main/java/com/agency/courseplatform/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.agency.courseplatform.repository;
+
+import com.agency.courseplatform.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface UserRepository extends JpaRepository<User, Long>
+{
+    
+}
+
